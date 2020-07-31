7867731868ba35e02a9d640bf43c7612d96b6ce6
CryptoWoody <cryptowoody@icloud.com>
2020-07-31 13:28:08 +0100

    Added position logging
    Added position logging to make win/loss analysis easier. This will be improved further when Buy/Sell functions are added.

--- src/BittrexBotTrader.ps1
+++ src/BittrexBotTrader.ps1
@@ -65,8 +65,10 @@
 ##region Main
     
     $Log = @()
+    $Positions = @()
 
     $LastLogItem = New-Object -TypeName PSObject
+    $Position = New-Object -TypeName PSObject
 
     $MarketInfo = Get-BittrexMarkets | Where-Object {$_.MarketName -eq $Market}
 
@@ -167,6 +169,10 @@
                     $Action = "BUY"
                     $OpenPosition = $true
 
+                    $Position = New-Object -TypeName PSObject
+                    $Position | Add-Member -MemberType NoteProperty -TypeName [Int] -Name "BuyTime" -Value $CycleStart
+                    $Position | Add-Member -MemberType NoteProperty -TypeName [Int] -Name "BuyPrice" -Value $MarketTicker.Bid
+
                 } else {
 
                     # Hold
@@ -182,6 +188,14 @@
                     # Sell
                     $Action = "SELL"
                     $OpenPosition = $false
+
+                    $Position | Add-Member -MemberType NoteProperty -TypeName [Int] -Name "SellTime" -Value $CycleStart
+                    $Position | Add-Member -MemberType NoteProperty -TypeName [Int] -Name "SellPrice" -Value $MarketTicker.Ask
+                    $Position | Add-Member -MemberType NoteProperty -TypeName [Int] -Name "PriceDiff" -Value ($Position.SellPrice - $Position.BuyPrice)
+                    $Positions += $Position
+
+                    $Position | Select-Object -Property BuyTime, BuyPrice, SellTime, SellPrice, PriceDiff
+                    $Position | Export-Csv -Path "$($PSScriptRoot)\Logs\BittrexBottrader-Posotions-$($SessionUid).csv" -NoTypeInformation -Append
 
                 } else {
 
@@ -223,7 +237,5 @@
         $LogItem | Export-Csv -Path "$($PSScriptRoot)\Logs\BittrexBottrader-$($SessionUid).csv" -NoTypeInformation -Append
 
     } until ($Continue -eq $false)
-
-    
 
 #endregion
